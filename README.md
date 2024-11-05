c25710a9c9a71404e58fc368a2495ca720c5f71e
szymonw <handerdog3@gmail.com>
2024-11-05 20:53:36 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# testgit
+Im learning how to use git, doink random stuff :)
---
+++ bmi.py
@@ -0,0 +1,5 @@
+height = float( input("podaj swoj wzrost w m: "))
+weight = float( input("podaj swoja wage w kg: "))
+
+BMI = weight / (height**2)
+print("Twoje BMI wynosi: " + str(BMI))
